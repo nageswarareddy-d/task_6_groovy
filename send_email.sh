f958e8fc527c96554a2784d49dda19c92f9ee671
Nageswara Reddy <nagesh@Nageswaras-MacBook-Air.local>
2020-06-30 19:04:58 +0530

    second commit

--- send_email.sh
+++ send_email.sh
@@ -1,1 +1,10 @@
-echo "Application is Not Working" |mail -s "Build Failed" nageswarareddy.d@gmail.com
+application_ip=$(kubectl get nodes -o jsonpath={.items[*].status.addresses[?\(@.type==\"InternalIP\"\)].address})
+port=$(kubectl get service pv-pod -o jsonpath="{.spec.ports[:].nodePort}")
+test_status=$(curl -s -o /dev/null -w "%{http_code}" http://$application_ip:$port)
+if [ "$test_status" -eq 200 ]; then
+	echo "Testing Successful” |mail -s "Build Success” nageswarareddy.d@gmail.com
+	exit 0
+else 
+	echo "Testing Failed. Reason For Failure: $test_status ” |mail -s "Build Failed" nageswarareddy.d@gmail.com
+    exit 1    
+fi  
